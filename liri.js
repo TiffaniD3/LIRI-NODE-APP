e80d41eb081d6dd5cf3b5dbfa8f9b720602a7985
TiffaniD3 <tkd93@me.com>
2019-02-15 00:26:07 -0600

    fixed spotify search

--- liri.js
+++ liri.js
@@ -1,22 +1,22 @@
 require("dotenv").config();
 let axios = require("axios");
-const fs = require("fs");
-const moment = require("moment");
+let moment = require("moment");
 let keys = require("./keys.js");
 let Spotify = require("node-spotify-api");
 let spotify = new Spotify(keys.Spotify);
-let figgy = require("figlet");
+let figlet = require("figlet");
 let chalk = require("chalk");
 let log = console.log;
 
 let command = process.argv[2];
 let param = process.argv[3];
 
-function switchCase() {
+// App commands
+function heyLiri() {
     switch (command) {
 
         case "spotify-this-song":
-        spoitfySong(param);
+        spotifySong(param);
         break;
 
         case "concert-this":
@@ -34,13 +34,13 @@ function switchCase() {
         default:
         log("LIRI is very particular. Please ensure you are using the proper commands.");
 
-    }
+    };
 };
 
 // Spotify function
 let asciiFig = "Spotify"
 
-function spoitfySong(param) {
+function spotifySong(param) {
 
     var songSearch;
     if (param === undefined) {
@@ -49,33 +49,34 @@ function spoitfySong(param) {
         songSearch = param;
     };
 
-    figgy(asciiFig, function(err, data){
+    // Spotify Header
+    figlet(asciiFig, function(err, data){
         if (err){
             log(err);
             return;
         }
         log(chalk.green(data));
+        log(chalk.green("===================================="));
     });
 
+    // Data pulled from Spotify
     spotify.search({ type: "track", query: songSearch})
-
     .then(function(response) {
-        var data = response.tracks.items;
-        log("This is a test", data[1].album.name);
-         for (let i = 0; i < data.length; i++) {
-        //     log("NUMBER: ", i + 1, "/", data.length);
-        //     log("Artist(s): ", + data[i].artists[0].name);
-        //     log("Song Name: ", + data[i].name);
-        //     log("Preview Song: ", + data[i].preview_url);
-            log("Album: ", + data[i].album.name);
-        //     log("========================")
+        let data = response.tracks.items;
+        for (let i = 0; i < data.length; i++){
+            log("Artist(s): ",  data[i].artists[0].name);
+            log("Song: ",  data[i].name);
+            log("Album: ",  data[i].album.name);
+            log("Preview: ",  data[i].preview_url);
+            log(chalk.green("===================================="));
         }
-        
     })
     .catch(function(err){
 
         log("I'm sorry, I wasn't able to find that. Here's why: " + err);
-    })
-}
+    });
+};
 
-switchCase();
+
+
+heyLiri();
