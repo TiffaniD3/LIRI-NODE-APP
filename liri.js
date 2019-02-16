9e9bcc636daa7dd4b1632fc2f9ffbb8e89e96f64
TiffaniD3 <tkd93@me.com>
2019-02-16 16:18:32 -0600

    fixed OMDB function

--- liri.js
+++ liri.js
@@ -19,30 +19,25 @@ function heyLiri() {
         spotifySong(param);
         break;
 
+        case "movie-this":
+        omdb(param);
+        break;
+
         case "concert-this":
         bandsInTown(param);
         break;
 
-        case "moive-this":
-        omdb(param);
-        break;
-
-        case "do-what-it-says":
-        getRandom();
-        break;
-
         default:
-        log("LIRI is very particular. Please ensure you are using the proper commands.");
+        log("LIRI is very particular. Please ensure you are using the proper command.");
 
     };
 };
 
 // Spotify function
-let asciiFig = "Spotify"
 
 function spotifySong(param) {
 
-    var songSearch;
+    let songSearch;
     if (param === undefined) {
         songSearch = "Ace of Base The Sign";
     } else {
@@ -50,7 +45,8 @@ function spotifySong(param) {
     };
 
     // Spotify Header
-    figlet(asciiFig, function(err, data){
+    let figFont = "Spotify"
+    figlet(figFont, function(err, data){
         if (err){
             log(err);
             return;
@@ -76,6 +72,84 @@ function spotifySong(param) {
         log("I'm sorry, I wasn't able to find that. Here's why: " + err);
     });
 };
+
+// OMDB Function
+
+function omdb(param) {
+
+    let movieSearch;
+    if (param === undefined) {
+        movieSearch = "Mr. Nobody";
+    } else {
+        movieSearch = param;
+    };
+
+    // OMDB Header
+    let figFont = "OMDB"
+    figlet(figFont, function(err, data){
+        if (err){
+            log(err);
+            return;
+    }
+    log(chalk.red(data));
+    log(chalk.red("===================================="));
+});
+
+    // Data pulled from OMDB
+    let queryURL = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryURL)
+    .then(function(response){
+        log("Movie Title: " + response.data.Title);
+        log("Year Released: " + response.data.Year);
+        log("IMDB Rating: " + response.data.imdbRating);
+        log("Rotten Tomatoes Score: " + response.data.Ratings[1].Value);
+        log("Country of Origin: " + response.data.Country);
+        log("Language: " + response.data.Language);
+        log("Plot: " + response.data.Plot);
+        log("Actors: " + response.data.Actors);
+        log(chalk.red("===================================="));
+    })
+    .catch(function(error){
+        log("I'm sorry, I wasn't able to find that. Here's why: " + error);
+    });
+
+};
+
+// BandsInTown Function
+function bandsInTown(param) {
+
+    let bandSearch;
+    if (param === undefined) {
+        bandSearch = "Rob Zombie";
+    } else {
+        bandSearch = param;
+    }
+
+    let figFont = "BandsInTown"
+    figlet(figFont, function(err, data){
+        if(err){
+            log(err);
+            return;
+        }
+        log(chalk.blue(data));
+    })
+
+    let queryURL = "https://rest.bandsintown.com/artists/" + bandSearch + "/events?app_id=codingbootcamp";
+    axios.get(queryURL)
+    .then(function(response){
+        for (var i = 0; i < response.data.length; i++){    
+        log("Artist: " + response.data[i].lineup);
+        log("Venue: " + response.data[i].venue.name);
+        log("Location: " + response.data[i].venue.city);
+        log(moment("Date: " + response.data[i].datetime)).format("MM/DD/YYYY");
+
+    }
+})
+    .catch(function(error){
+        log("I'm sorry, I wasn't able to find that. Here's why: " + error);
+    });
+
+} 
 
 
 
